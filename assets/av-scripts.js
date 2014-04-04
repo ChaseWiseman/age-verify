7ea0a8195d2bdec147f23015ad3d943aa33cb15c
Matthew Kosoy <matt.kosoy@gmail.com>
2014-04-04 08:44:45 -0700

    added fix that disabed scrolling of the site while the age-gate is visible

--- assets/av-scripts.js
+++ assets/av-scripts.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function($) {
-		// if(jQuery('#av-overlay-wrap').is(":visible")){
-		// 	$('html, body').css('overflow: hidden;');
-		// } else {
-		// 	jQuery('html, body').css('overflow:scroll');
-		// }
+		if(jQuery('#av-overlay-wrap').is(":visible")){
+			jQuery('html, body').css('overflow', 'hidden');
+		} else {
+			jQuery('html, body').css('overflow', 'scroll');
+		}
 
 		jQuery('#av_verify_link').click(function(event){
 		    event.preventDefault();
